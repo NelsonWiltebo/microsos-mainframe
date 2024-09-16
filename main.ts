33bc0edcfce18652adc7181f1db1c8e1a96fb6eb
NelsonWiltebo <105360288+NelsonWiltebo@users.noreply.github.com>
2024-09-16 15:22:13 +0200

    base

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
+function getMax (array: any[]) {
+    max = 0
+    for (let value of array) {
+        if (value > max) {
+            max = value
+        }
+    }
+    return max
+}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "microbitActive") {
-    	
+        activeIDs.unshift(getMax(activeIDs))
+        radio.sendValue("setID", getMax(activeIDs))
     }
 })
-let ID = 0
+let max = 0
 let activeIDs: number[] = []
+radio.setGroup(57)
+let ID = 0
+activeIDs = []
 basic.forever(function () {
 	
 })
