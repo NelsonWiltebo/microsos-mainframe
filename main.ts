06d96c69ea94882666d13a3a57a7b4f5b3aa1fa4
NelsonWiltebo <105360288+NelsonWiltebo@users.noreply.github.com>
2024-09-16 14:44:02 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "microbitActive") {
+    	
+    }
+})
+let ID = 0
+let activeIDs: number[] = []
+basic.forever(function () {
+	
+})
