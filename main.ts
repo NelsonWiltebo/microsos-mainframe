fc5508cdbc55d89411c85ca78a237f7dc17f85aa
NelsonWiltebo <105360288+NelsonWiltebo@users.noreply.github.com>
2024-09-16 17:27:51 +0200

    Mainframe base

--- main.ts
+++ main.ts
@@ -7,17 +7,54 @@ function getMax (array: any[]) {
     }
     return max
 }
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "microbitActive") {
-        activeIDs.unshift(getMax(activeIDs))
-        radio.sendValue("setID", getMax(activeIDs))
+function getMin (array: any[]) {
+    min = 0
+    for (let value of array) {
+        if (value < min) {
+            min = value
+        }
+    }
+    return min
+}
+function addID (num: number) {
+    activeIDs.unshift(num)
+    radio.sendValue("setID", num)
+}
+radio.onReceivedValue(function (name, value) {
+    if (name.includes("_id:" + convertToText(ID))) {
+        if (name.includes("microbitActive")) {
+            if (value == 0) {
+                addID(getMax(activeIDs) + 1)
+            } else if (value == 1) {
+                addID(getMax(activeIDs) - 1)
+            }
+        } else if (name.includes("hasFallen")) {
+            if (activeIDs.indexOf(value) != -1) {
+                allHelp.push(value)
+            }
+        } else if (name.includes("acceptHelp")) {
+            if (activeIDs.indexOf(value) != -1) {
+                allHelp.removeAt(activeIDs.indexOf(value))
+                radio.sendValue("isHelping_id:-1", value)
+            }
+        } else if (name.includes("hasHelp")) {
+            radio.sendString("recieveHelp_id:" + value)
+        }
     }
 })
+let min = 0
 let max = 0
+let allHelp: number[] = []
 let activeIDs: number[] = []
-radio.setGroup(57)
 let ID = 0
-activeIDs = []
+radio.setGroup(57)
+ID = 0
+activeIDs = [ID]
+allHelp = []
 basic.forever(function () {
-	
+    if (allHelp.length > 0) {
+        for (let value of allHelp) {
+            radio.sendValue("fallHelp_id:-1", value)
+        }
+    }
 })
